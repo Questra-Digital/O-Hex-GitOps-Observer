31a52e8ca39a5d57792be58a62a155aa20af5d7e
Ali Qumail <aliqumail@gmail.com>
2022-12-05 20:42:08 +0500

    Redis is connected and working

--- app/backend/GoBackendServiceProject/go.mod
+++ app/backend/GoBackendServiceProject/go.mod
@@ -12,6 +12,7 @@ require (
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gomodule/redigo v1.8.9 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- app/backend/GoBackendServiceProject/server/main.go
+++ app/backend/GoBackendServiceProject/server/main.go
@@ -13,6 +13,7 @@ import (
     "go.mongodb.org/mongo-driver/mongo/options"
     "go.mongodb.org/mongo-driver/mongo/readpref"
     "github.com/joho/godotenv"
+	"github.com/gomodule/redigo/redis"
 )
 
 
@@ -27,7 +28,43 @@ type movieServer struct {
 	pb.UnimplementedMovieServer
 }
 
+
+var pool = newPool()
+
 func main() {
+
+	// REDIS CONNECTION 
+
+	Rclient := pool.Get()
+	defer Rclient.Close()
+
+	// _, err := Rclient.Do("SET", "mykey", "Hello from redigo!")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// value, err := Rclient.Do("GET", "mykey")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("%s \n", value)
+
+	// _, err = Rclient.Do("ZADD", "vehicles", 4, "car")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// _, err = Rclient.Do("ZADD", "vehicles", 2, "bike")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// vehicles, err := Rclient.Do("ZRANGE", "vehicles", 0, -1, "WITHSCORES")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("%s \n", vehicles)
+
  
     // MONGODB CONNECTION 
     envMap, mapErr := godotenv.Read(".env")
@@ -143,3 +180,17 @@ func (s *movieServer) DeleteMovie(ctx context.Context,
 
 	return &res, nil
 }
+
+func newPool() *redis.Pool {
+	return &redis.Pool{
+	   MaxIdle: 80,
+	   MaxActive: 12000,
+	   Dial: func() (redis.Conn, error) {
+			 c, err := redis.Dial("tcp", ":6379")
+			 if err != nil {
+				 panic(err.Error())
+			 }
+			 return c, err
+		 },
+	 }
+ }
