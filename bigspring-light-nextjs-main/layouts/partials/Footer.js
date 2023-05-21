1b5606f8869c6b9a26d2dbfe6db09fc33cedf27b
Fateh Ali <i190648@lhr.nu.edu.pk>
2023-05-21 22:34:57 +0500

    homepage template

---
+++ bigspring-light-nextjs-main/layouts/partials/Footer.js
@@ -0,0 +1,23 @@
+import Social from "@components/Social";
+import config from "@config/config.json";
+import menu from "@config/menu.json";
+import social from "@config/social.json";
+import { markdownify } from "@lib/utils/textConverter";
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  const { copyright, footer_content } = config.params;
+  const { footer } = menu;
+  return (
+    <footer className="section bg-theme-light pb-0">
+      <div className="container">
+        {/* footer menu */}
+        
+        
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ bigspring-light-nextjs-main/pages/index.js
@@ -0,0 +1,81 @@
+import config from "@config/config.json";
+import Base from "@layouts/Baseof";
+import Cta from "@layouts/components/Cta";
+import { markdownify } from "@lib/utils/textConverter";
+import Image from "next/image";
+import Link from "next/link";
+import { Autoplay, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import { getListPage } from "../lib/contentParser";
+
+const Home = ({ frontmatter }) => {
+  const { banner, feature, services, workflow, call_to_action } = frontmatter;
+  const { title } = config.site;
+
+  return (
+    <Base title={title}>
+      {/* Banner */}
+      <section className="section pb-[50px]">
+        <div className="container">
+          <div className="row text-center">
+            <div className="mx-auto lg:col-10">
+              <h1 className="font-primary font-bold">{banner.title}</h1>
+              <p className="mt-4">{markdownify(banner.content)}</p>
+              {banner.button.enable && (
+                <Link
+                  className="btn btn-primary mt-4"
+                  href={banner.button.link}
+                  rel={banner.button.rel}
+                >
+                  {banner.button.label}
+                </Link>
+              )}
+              <Image
+                className="mx-auto mt-12"
+                src={banner.image}
+                width={750}
+                height={390}
+                alt="banner image"
+                priority
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* workflow */}
+      <section className="section pb-0">
+        <div className="mb-8 text-center">
+          {markdownify(
+            workflow.title,
+            "h2",
+            "mx-auto max-w-[400px] font-bold leading-[44px]"
+          )}
+          {markdownify(workflow.description, "p", "mt-3")}
+        </div>
+        <Image
+          src={workflow.image}
+          alt="workflow image"
+          width={1920}
+          height={296}
+        />
+      </section>
+
+      {/* Cta */}
+      <Cta cta={call_to_action} />
+    </Base>
+  );
+};
+
+export const getStaticProps = async () => {
+  const homePage = await getListPage("content/_index.md");
+  const { frontmatter } = homePage;
+  return {
+    props: {
+      frontmatter,
+    },
+  };
+};
+
+export default Home;
